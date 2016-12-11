70ed2acd0ed7218324c1c20b9fecc5ee942bb96c
Martin Markech <martin.markech@matho.sk>
2016-12-11 17:02:17 +0100

    add sortable to countries

---
+++ app/controllers/admin/countries/index.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  queryParams: ['sortBy', 'sortDir'],
+
+  numberOfRecords: 100,
+
+  // Using for sortBy direction
+  id: false,
+  title: false,
+  code: false,
+  // Default sorting
+  sortBy: 'id',
+  sortDir: 'asc',
+
+  sortProperties: Ember.computed('sortBy', 'sortDir', function() {
+    return [`${this.get('sortBy')}:${this.get('sortDir')}`];
+  }),
+
+  sortedCountries: Ember.computed.sort('model', 'sortProperties'),
+
+
+  actions: {
+
+// Called when the table header clicked
+    sortBy(prop) {
+
+      // Change the boolean, we use this value for returning desc or asc
+      this.toggleProperty(prop);
+      let direction = this.get(prop) ? 'desc' : 'asc';
+
+      this.set('sortBy', prop);
+      this.set('sortDir', direction);
+    },
+
+  }
+});
